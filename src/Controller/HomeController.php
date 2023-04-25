4a77b2462df26a5f8e9ebada25c0ef60f662edd1
NikoScott <nico63@live.fr>
2023-04-25 09:55:55 +0200

    création des controleurs et des vues

---
+++ src/Controller/HomeController.php
@@ -0,0 +1,23 @@
+<?php
+
+namespace App\Controller;
+
+use Symfony\Bundle\FrameworkBundle\Controller\AbstractController;
+use Symfony\Component\HttpFoundation\Response;
+use Symfony\Component\Routing\Annotation\Route;
+
+class HomeController extends AbstractController
+{
+    #[Route('/home', name: 'app_home')]
+    public function index(): Response // le retour de la méthode index() est un objet de type réponse
+    // index() va intercepter une requête et retourner une réponse
+    // en php 8 on peut typer le retour des méthodes
+    {   
+        // elle retourne l'appel à la méthode render()
+        // render() => renvoit une vue
+        // avec un tableau de paramètres
+        return $this->render('home/index.html.twig', [
+            'controller_name' => 'HomeController',
+        ]);
+    }
+}
