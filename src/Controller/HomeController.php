fa601c9f4038491a8394dc0f84263092c43e420b
Nicolas PEREIRE <nico63@live.fr>
2023-05-02 16:57:04 +0200

    affichage img article, img assets, pagination, page dynamique

--- src/Controller/HomeController.php
+++ src/Controller/HomeController.php
@@ -5,14 +5,16 @@ namespace App\Controller;
 use App\Entity\Articles;
 use App\Entity\Category;
 use Doctrine\ORM\EntityManagerInterface;
+use Knp\Component\Pager\PaginatorInterface;
 use Symfony\Bundle\FrameworkBundle\Controller\AbstractController;
+use Symfony\Component\HttpFoundation\Request;
 use Symfony\Component\HttpFoundation\Response;
 use Symfony\Component\Routing\Annotation\Route;
 
 class HomeController extends AbstractController
 {
     #[Route('/', name: 'app_home')]
-    public function index(EntityManagerInterface $entityManager): Response // le retour de la méthode index() est un objet de type réponse
+    public function index(EntityManagerInterface $entityManager, PaginatorInterface $paginator, Request $request): Response // le retour de la méthode index() est un objet de type réponse
     // index() va intercepter une requête et retourner une réponse
     // en php 8 on peut typer le retour des méthodes
     {   
@@ -24,6 +26,13 @@ class HomeController extends AbstractController
         // et j'ai appelé la méthode findAll()
         $articles = $entityManager->getRepository(Articles::class)->findAll();
         $categories = $entityManager->getRepository(Category::class)->findAll();
+
+        // je remplace article par pagination
+        $articles = $paginator->paginate(
+            $articles, /* query NOT result */
+            $request->query->getInt('page', 1), /*page number*/
+            4 /*limit per page*/
+    );
 
         return $this->render('home/index.html.twig', [
             'listArticles' => $articles, // je passe des parametres à ma vue
